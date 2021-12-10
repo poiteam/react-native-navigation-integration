a08bb28189bc7ea375f173262611fabf860e326e
Mohammad Khakpaki <mohammad@poilabs.com>
2021-12-10 13:14:33 +0300

    Implement android navigation sdk

--- android/app/src/main/java/com/reactnativenavigationintegration/HomeActivity.java
+++ android/app/src/main/java/com/reactnativenavigationintegration/HomeActivity.java
@@ -15,6 +15,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.poilabs.navigation.model.PoiNavigation;
+import com.poilabs.navigation.model.PoiSdkConfig;
 import com.poilabs.navigation.view.fragments.MapFragment;
 
 import androidx.fragment.app.Fragment;
@@ -27,9 +28,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import org.jetbrains.annotations.Nullable;
 
-import getpoi.com.poibeaconsdk.PoiScanner;
 
 import static androidx.core.app.ActivityCompat.requestPermissions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -44,33 +47,33 @@ public class HomeActivity extends AppCompatActivity {
 
         startNavigation();
 
-//        PoiScanner.Config config = new PoiScanner.Config(secretId,uniqueId,appId);
-//        config.setEnabled(true);
-//        config.setOpenSystemBluetooth(false);
-//
-//        PoiScanner.init(config,this);
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            askLocalPermission();
-//        } else {
-//            PoiScanner.startScan(this);
-//        }
-
     }
 
     private void startNavigation() {
-        PoiNavigation poiNavigation = new PoiNavigation();
-        poiNavigation.init(this, appId, secretId, uniqueId, "tr", false);
-        poiNavigation.bind((PoiNavigation.OnNavigationReady)(new PoiNavigation.OnNavigationReady() {
-            public void onReady(@Nullable MapFragment mapFragment) {
-                if (mapFragment != null) {
-                    Bundle bundle = new Bundle();
-                    bundle.putString("storeId", "AVB1160");
-                    mapFragment.setArguments(bundle);
-                    HomeActivity.this.getSupportFragmentManager().beginTransaction().replace(R.id.mapLayout, (Fragment)mapFragment).commitAllowingStateLoss();
-                }
+        String localeLanguage = "tr";
+
+
+        PoiSdkConfig poiSdkConfig = new PoiSdkConfig(
+                appId,
+                secretId,
+                uniqueId
+        );
+        PoiNavigation.getInstance(
+                this,
+                localeLanguage,
+                poiSdkConfig
+        ).bind(new PoiNavigation.OnNavigationReady() {
+            @Override
+            public void onReady(MapFragment mapFragment) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.mapLayout, mapFragment).commitAllowingStateLoss();
             }
-        }));
+
+            @Override
+            public void onStoresReady() {
+                PoiNavigation.getInstance().showPointsOnMap(Arrays.asList("AVB1160"));
+            }
+        });
+
     }
 
 
@@ -81,7 +84,7 @@ public class HomeActivity extends AppCompatActivity {
             requestPermissions(new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
 
         } else {
-            PoiScanner.startScan(this);
+            startNavigation();
         }
     }
 
