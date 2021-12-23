8ce2da167b5cf79a13851fa763e1abd362981ea0
emre kuru <emre@poilabs.com>
2021-12-23 17:59:13 +0300

    android native view completed

--- App.js
+++ App.js
@@ -7,6 +7,7 @@
  */
 
 import React from 'react';
+import { useEffect, useRef } from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -17,13 +18,21 @@ import {
   useColorScheme,
   View,
   NativeModules,
-  Button
+  Button,
+  PixelRatio
 } from 'react-native';
 
 import MapView from './MapView.js';
 import {
   Colors
 } from 'react-native/Libraries/NewAppScreen';
+
+import { PoiMapViewManager } from './PoiMapViewManager';
+import { UIManager, findNodeHandle } from 'react-native';
+
+if(Platform.OS === 'ios') {
+
+}
 
 
 
@@ -34,28 +43,68 @@ const App: () => Node = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  return (
-    <View style={{flex: 1}}>
-        <View style={{height: 100, backgroundColor:"blue"}}/>
-        <MapView language={"en"} getRouteTo={"store_id"} showOnMap={"store_id"} style={{ flex: 1}}/>
-        <View style={{height: 100, backgroundColor:"blue"}}>
-        <Button
-          title="Show pin on map"
-          onPress={() => {
-            NativeModules.PoilabsNavigationBridge.showPointOnMap("store_id");
-          }
-          }
-        />
+
+
+  if(Platform.OS === 'ios') {
+    return (
+      <View style={{flex: 1}}>
+          <View style={{height: 100, backgroundColor:"blue"}}/>
+          <MapView language={"en"} getRouteTo={"store_id"} showOnMap={"store_id"} style={{ flex: 1}}/>
+          <View style={{height: 100, backgroundColor:"blue"}}>
           <Button
-          title="Get route"
-          onPress={() => {
-            NativeModules.PoilabsNavigationBridge.getRouteTo("store_id");
-          }
-          }
-        />
-        </View>
-    </View>
+            title="Show pin on map"
+            onPress={() => {
+              NativeModules.PoilabsNavigationBridge.showPointOnMap("store_id");
+            }
+            }
+          />
+            <Button
+            title="Get route"
+            onPress={() => {
+              NativeModules.PoilabsNavigationBridge.getRouteTo("store_id");
+            }
+            }
+          />
+          </View>
+      </View>
+    );
+  } else {
+    const createFragment = (viewId) =>
+  UIManager.dispatchViewManagerCommand(
+    viewId,
+    // we are calling the 'create' command
+    UIManager.PoiMapViewManager.Commands.create.toString(),
+    [viewId]
   );
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const viewId = findNodeHandle(ref.current);
+    createFragment(viewId);
+  }, []);
+    return (
+      <View style={{flex: 1}}>
+          <View style={{height: 100, backgroundColor:"blue"}}/>
+          <PoiMapViewManager
+          showPointOnMap = "store_id"
+          language = "en"
+        style={{
+          // converts dpi to px, provide desired height
+          height: PixelRatio.getPixelSizeForLayoutSize(500),
+          // converts dpi to px, provide desired width
+          width: PixelRatio.getPixelSizeForLayoutSize(500)
+        }}
+        ref={ref}
+      />
+          <View style={{height: 100, backgroundColor:"blue"}}>
+          </View>
+      </View>
+    );
+  }
+
+
+
 };
 
 const styles = StyleSheet.create({
