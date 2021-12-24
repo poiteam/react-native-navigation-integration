e69fe7d561582c5fccbbf79cd5f829c2cc155b7c
emre kuru <emre@poilabs.com>
2021-12-24 11:05:17 +0300

    android view files are seperated

---
+++ PoiMapView.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef } from 'react';
+import { UIManager, findNodeHandle, PixelRatio } from 'react-native';
+
+import { PoiMapViewManager } from './PoiMapViewManager';
+
+const createFragment = (viewId) =>
+  UIManager.dispatchViewManagerCommand(
+    viewId,
+    // we are calling the 'create' command
+    UIManager.PoiMapViewManager.Commands.create.toString(),
+    [viewId]
+  );
+
+export const PoiMapView = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const viewId = findNodeHandle(ref.current);
+    createFragment(viewId);
+  }, []);
+
+  return (
+    <PoiMapViewManager
+      style={{
+        flex: 1
+      }}
+      ref={ref}
+    />
+  );
+};
